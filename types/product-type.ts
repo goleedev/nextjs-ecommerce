fe2f53fc06f8bc4e7d0eb4c67f61db73288c8c33
goleedev <golee.dev@gmail.com>
2021-10-01 20:44:03 +0900

    1.0 Add Product Type

---
+++ types/product-type.ts
@@ -0,0 +1,10 @@
+export default interface ProductType {
+  key?: number
+  id: number
+  product: string
+  quantity?: number
+  category: string
+  image: string
+  price: number
+  freeShipping: boolean
+}
