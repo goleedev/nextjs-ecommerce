224a27618637bc349ad290faac083788eef5a287
goleedev <golee.dev@gmail.com>
2021-10-02 19:23:57 +0900

    9.0 Update Cart Page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { removeFromCart } from 'state/cart.slice'
 import { ReducerType } from 'state/cart.slice'
 import ProductType from 'types/product-type'
+import trashIcon from 'assets/icon/Trash.svg';
 import styles from 'styles/CartPage.module.css'
 
 const CartPage = () => {
@@ -23,21 +24,37 @@ const CartPage = () => {
           <div className={styles.header}>
             <h1>홍길동님의 장바구니</h1>
           </div>
-          {cart.map((item: ProductType, index: number) => (
-            <div key={index} className={styles.body}>
-              <div className={styles.image}>
-                <Image src={item.image} height="90" width="65" />
-              </div>
-              <p>{item.product}</p>
-              <p>$ {item.price}</p>
-              <p>{item.quantity}</p>
-              <div className={styles.buttons}>
-                <button onClick={() => dispatch(removeFromCart(item.id))}>x</button>
-              </div>
-              <p>$ {(item.quantity ?? 0) * item.price}</p>
+          <div className={styles.body}>
+            <div className={styles.cartItems}>
+              {cart.map((item: ProductType, index: number) => (
+                <div key={index} className={styles.item}>
+                  <div className={styles.image}>
+                    <Image src={item.image ?? ''} alt={'product-image'} width={'100%'} height={'100%'} />
+                  </div>
+                  <div className={styles.detail}>
+                    <p>{item.product}</p>
+                    <p><b>{item.freeShipping ? '무료배송' : '유료배송'}</b> (업제직배송)</p>
+                    <p>가격 <b>{item.price.toLocaleString('ko-KR')}</b>원 (할인 50% 적용)</p>
+                    <p>총 <b>{item.quantity}</b> 개</p>
+                  </div>
+                  <div className={styles.buttons}>
+                    <button onClick={() => dispatch(removeFromCart(item))}>
+                      <Image src={trashIcon} alt={'delete'} width={24} height={24} />
+                    </button>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-          <h2>Grand Total: $ {getTotalPrice()}</h2>
+            <div className={styles.total}>
+              <div className={styles.totalContent}>
+                <p><span>총 상품금액</span> <span>{getTotalPrice().toLocaleString('ko-KR')}원</span></p>
+                <p><span>총 배송비</span> <span>0원</span></p>
+                <p><span>총 할인금액</span> <span>{getTotalPrice().toLocaleString('ko-KR')}원</span></p>
+                <p><span>결제금액</span> <span>{getTotalPrice().toLocaleString('ko-KR')}원</span></p>
+              </div>
+              <button>{cart.length}개 상품 구매하기</button>
+            </div>
+          </div>
         </>
       )}
     </div>
