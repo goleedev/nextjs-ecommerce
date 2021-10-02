13014936b1391a0284f06946ee4b24e40d18359d
goleedev <golee.dev@gmail.com>
2021-10-02 18:10:41 +0900

    7.1 Add New Releases Page

---
+++ pages/new-releases.tsx
@@ -0,0 +1,28 @@
+import ProductCard from 'components/ProductCard';
+import { getProducts } from 'pages/api/products';
+import ProductType from "types/product-type";
+import styles from 'styles/NewReleasesPage.module.css';
+
+interface INewReleasesPage {
+  products: ProductType[]
+}
+
+const NewReleasesPage = ({ products }: INewReleasesPage) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>All Results</h1>
+      <div className={styles.cards}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewReleasesPage;
+
+export async function getStaticProps() {
+  const products = await getProducts();
+  return { props: { products } };
+}
